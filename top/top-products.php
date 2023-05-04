be9e1458350336ab681d4bbda0f29497a22eb345
danil <2020_23@bk.ru>
2023-05-04 17:25:46 +0500

    без изменений

--- top/top-products.php
+++ top/top-products.php
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 <?require 'connect.php';
-$id_categ = $_GET['id_categ']; 
+$id_categ = $_GET['id']; 
 $query = "SELECT * FROM product ";     
 	$rs_result = mysqli_query ($conn, $query);    
    
@@ -21,27 +20,4 @@ $query = "SELECT * FROM product ";
 					</div>';
     }
     ?>
-=======
-<?require 'connect.php';
-$id_categ = $_GET['id_categ']; 
-$query = "SELECT * FROM product ";     
-	$rs_result = mysqli_query ($conn, $query);    
-   
-	while ($row = mysqli_fetch_array($rs_result)) 
-	{    
-	
-	echo ' 
-<div class="col-lg-3 mb-4 text-center">
-						<div class="product-entry border">
-							<a href="http://localhost/footwear/product-detail.php" class="prod-img">
-								<img src='.$row["product_image"].' class="img-fluid" alt="Free html5 bootstrap 4 template">
-							</a>
-							<div class="desc">
-								<h2><a href="#">'.$row["product_name"].'</a></h2>
-								<span class="price">'.$row["product_price"].' руб.</span>
-							</div>
-						</div>
-					</div>';
-    }
-    ?>
->>>>>>> aa366c3dc46c2e939a31e7c46e7336f3621fc6e0
+
