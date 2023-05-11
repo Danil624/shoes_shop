51f8a9f7160f7eca189ffe2777defaa8c6686206
danil <2020_23@bk.ru>
2023-05-11 11:53:36 +0500

    категории

--- back_php/mens.php
+++ back_php/mens.php
@@ -1,17 +1,8 @@
-<?php  	  	
-	require 'connect.php';
-    $id_categ = $_GET['id_categ']; 
-	$query = "SELECT * FROM categ WHERE id = ".$id_categ;     
-	$rs_result = mysqli_query ($conn, $query);    
-	while ($row = mysqli_fetch_array($rs_result)) 
-	{    
-	echo '<title>Товары для категории '.$row["title_cat"].'</title>';
-	}
-?> 
+
 <?
 require 'connect.php';
 $id_categ = $_GET['id_categ']; 
-$query = "SELECT * FROM product WHERE categ=".$id_categ;     
+$query = "SELECT * FROM product WHERE cat=".$id_categ;     
 	$rs_result = mysqli_query ($conn, $query);    
    
 	while ($row = mysqli_fetch_array($rs_result)) 
@@ -21,7 +12,7 @@ $query = "SELECT * FROM product WHERE categ=".$id_categ;
 <div class="col-md-3 col-lg-3 mb-4 text-center">
 <div class="product-entry border">
     <a href="#" class="prod-img">
-        <img src="'.$row["product_image"].'" class="img-fluid" alt="Free html5 bootstrap 4 template">
+        <img src="'.$row["product_image"].'" class="img-fluid" ">
     </a>
     <div class="desc">
         <h2><a href="#">'.$row["product_name"].'</a></h2>
